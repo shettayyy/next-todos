7a6655fdc9ceb745d3e304e64cf87ad14b60d327
Rahul Shetty <shettyrahul8june@gmail.com>
2024-05-11 19:59:25 +0530

    feat: configure font for the project

---
+++ apps/task-master-ui/src/app/app.tsx
@@ -0,0 +1,13 @@
+// import { Route, Routes, Link } from 'react-router-dom';
+export function App() {
+  return (
+    <div className="font-poppins font-normal">
+      <h1>
+        <span> Hello there, </span>
+        Welcome task-master-ui 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/task-master-ui/tailwind.config.js
@@ -0,0 +1,22 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
